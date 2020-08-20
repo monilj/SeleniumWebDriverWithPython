a87bbfac091f47a5937fae9944c2f2dfb6ffe0ef
Monil Joshi <monil.joshi@qualitia.local>
2020-08-20 23:20:10 +0530

    Added code for Basic Syntax | Numbers and Math operation

---
+++ BasicPytthonSyntax/NumberAndMathOperationsInPython.py
@@ -0,0 +1,28 @@
+int_num = 10
+float_num = 20.0
+
+print(int_num)
+print(float_num)
+
+num1 = 10
+num2 = 20
+
+addition = num1 + num2
+print(addition)
+
+subtraction = num2 - num1
+print(subtraction)
+
+multiplication = num1 * num2
+print(multiplication)
+
+division = num2 / num1
+print(division)
+
+num3 = 8
+num4 = 3
+modulus =num3 % num4
+print(modulus)
+
+exponential = num3 ** num4
+print(exponential)
--- BasicPytthonSyntax/VariablesInPython.py
+++ BasicPytthonSyntax/VariablesInPython.py
@@ -21,3 +21,9 @@ d = c
 
 print(c == d)
 print(d is c)
+
+# multiple assignment
+a = b = c = 1
+p, q, r = 1, 2, "john"
+
+print(a, b, c, p, q, r)
