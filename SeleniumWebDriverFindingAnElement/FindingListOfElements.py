c40a5e0545a46f8e59ab5732ec0721e95452747d
Monil Joshi <monil.joshi@qualitia.local>
2020-09-09 19:48:47 +0530

    Selenium WebDriver with Python |  Finding List of Elements

---
+++ SeleniumWebDriverFindingAnElement/FindingListOfElements.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+
+class FindingListOfElements():
+    def test(self):
+        baseUrl= "https://letskodeit.teachable.com/pages/practice"
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        driver.get(baseUrl)
+        elementListByClassName = driver.find_elements_by_class_name("inputs")
+        length1 = len(elementListByClassName)
+        if elementListByClassName is not None:
+            print("ClassName -> Size of the list is: " + str(length1))
+
+        elementListByTagName = driver.find_elements(By.TAG_NAME, "td")
+        length2 = len(elementListByTagName)
+
+        if elementListByTagName is not None:
+            print("TagName -> Size of the list is: " + str(length2))
+
+
+findingListOfElements = FindingListOfElements()
+findingListOfElements.test()
