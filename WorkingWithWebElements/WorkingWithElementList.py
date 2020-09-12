57ee62ac75379bf7599953701e287ca3105f6e3d
Monil Joshi <monil.joshi@qualitia.local>
2020-09-13 01:34:32 +0530

    Working with Web Elements | List of Element in same group

---
+++ WorkingWithWebElements/WorkingWithElementList.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+
+class WorkingWithElementList():
+    def test(self):
+        baseUrl = "https://letskodeit.teachable.com/pages/practice"
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        driver.get(baseUrl)
+        driver.implicitly_wait(10)
+
+        radioButtonsList = driver.find_elements(
+            By.XPATH, "//input[contains(@type,'radio') and contains(@name,'cars')]")
+        size = len(radioButtonsList)
+        print("Size of the list: " + str(size))
+
+        for radioButton in radioButtonsList:
+            isSelected = radioButton.is_selected()
+
+            if not isSelected:
+                radioButton.click()
+                time.sleep(2)
+        driver.quit()
+
+
+workingWithElementList = WorkingWithElementList()
+workingWithElementList.test()
