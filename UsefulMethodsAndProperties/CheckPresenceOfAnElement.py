e6e0206069651757728ec986f60858e1c5693497
Monil Joshi <monil.joshi@qualitia.local>
2020-09-16 12:53:21 +0530

    Useful Methods and Properties | Check Persence of an Element

---
+++ UsefulMethodsAndProperties/CheckPresenceOfAnElement.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from webdriver_manager.firefox import GeckoDriverManager
+
+from UsefulMethodsAndProperties.HandyWrappers import HandyWrappers
+
+
+class CheckPresenceOfAnElement():
+    def test(self):
+        baseUrl = "https://letskodeit.teachable.com/pages/practice"
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        hw = HandyWrappers(driver)
+        driver.get(baseUrl)
+        elementResult1 = hw.isElementPresent("name", By.ID)
+        print(str(elementResult1))
+
+        elementResult2 = hw.elementPresenceCheck("//input[@id='name1']", By.XPATH)
+        print(str(elementResult2))
+        driver.quit()
+
+
+checkPresenceOfAnElement = CheckPresenceOfAnElement()
+checkPresenceOfAnElement.test()
