c85bc8510c8f93641d252cf7484a170d0501bdef
Monil Joshi <monil.joshi@qualitia.local>
2020-09-19 23:39:52 +0530

    Unit Test Infrastructure | Working with Test Suite

---
+++ UnitTestInfrastructure/WorkingWithTestSuite.py
@@ -0,0 +1,12 @@
+import unittest
+from UnitTestInfrastructure.TestClass1InTestSuite import TestClass1
+from UnitTestInfrastructure.TestClass2InTestSuite import TestClass2
+
+# Get all tests from TestClass1 and TestClass2
+tc1 = unittest.TestLoader().loadTestsFromTestCase(TestClass1)
+tc2 = unittest.TestLoader().loadTestsFromTestCase(TestClass2)
+
+# Create a test suite combining TestClass1 and TestClass2
+smokeTest = unittest.TestSuite([tc1, tc2])
+
+unittest.TextTestRunner(verbosity=2).run(smokeTest)
