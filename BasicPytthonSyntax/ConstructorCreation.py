afb20c9e089bc1e4567e9f45dab6e1d88e623f19
Monil Joshi <monil.joshi@qualitia.local>
2020-12-07 22:09:10 +0530

    Added code for Basic Syntax

---
+++ BasicPytthonSyntax/ConstructorCreation.py
@@ -0,0 +1,9 @@
+class Constructor:
+    def __init__(self):
+        print("I am called automatically when object is created")
+
+    def normalFunction(self):
+        print("This is normal function")
+
+
+# Constructor().normalFunction()
