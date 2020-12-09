55565b9ab48ae20fd3f0de618c466b3f64d1583a
Monil Joshi <monil.joshi@qualitia.local>
2020-12-09 12:17:00 +0530

    Added Basic Syntax | Update Files in Python file with readLine and readLines() examples

--- BasicPytthonSyntax/FilesInPython.py
+++ BasicPytthonSyntax/FilesInPython.py
@@ -22,7 +22,6 @@ Reading line by line -> .readline()
 """
 
 my_file = open("firstfile.txt", 'r')
-
 print(str(my_file.read()))
 my_file.close()
 
@@ -56,3 +55,22 @@ print()
 print("With As Read Start")
 with open("withas.txt", "r") as with_as_read:
     print(str(with_as_read.read()))
+
+print("======= This code is newly added =======")
+read_file = open("sample.txt", 'r')
+# print(str(read_file.read(7)))
+# print(str(read_file.read()))
+# Print line by ine using readLine Method
+
+print("=============== Print line by ine using readLine Methdd ===============")
+line = read_file.readline()
+while line != "":
+    print(line)
+    line = read_file.readline()
+my_file.close()
+
+print("=============== Print line by ine using readLineS Methdd ===============")
+read_file_for = open("sampleForReadlines.txt", 'r')
+for line in read_file_for.readlines():
+    print(line)
+read_file_for.close()
